18f2e8e60794f3d934f21e45c6ac36b347b34ab3
kaan <kabuke13@gmail.com>
2023-10-16 15:44:30 +0100

    lab changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,17 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  //The <NavBar> component should return the following JSX elements:
+    // a <nav> element
+    // inside the nav element, there should be two child elements:
+    // an <a> element, with the href attribute set to #home, and the content text set to "Home"
+    // an <a> element, with the href attribute set to #about, and the content text set to "About"
+  return(
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
+  ) 
 }
 
 export default NavBar;
